3797a20da6bc391f47591bd3f99298858b295a32
yangyu <yangyu2019@buaa.edu.cn>
2021-05-10 09:07:07 +0000

    增加注释

--- infer/eval_infer/eval_infer_base.py
+++ infer/eval_infer/eval_infer_base.py
@@ -11,10 +11,25 @@ from data_preprocess.Tokenizer.label_tokenizer import ClassificationLabelTokeniz
 # 评估推断方法基类（带标签）
 class EvalInfer:
     """评估推断方法基类
+
+    变量：
+    self.configs
+    self.model_name
+    self.model_path
+    self.eval_data_file
+    self.label2index_json_path
+    self.token2index_json_path
+    self.tokenizer
+    self.label_tokenizer
+    self.model
+
+    方法：
+    predict()
+    predict_one()
     """
     def __init__(self, configs: dict):
         
-        # 设置字典
+        # 加载参数字典
         self.configs = configs
         
         # 设置模型以及路径
@@ -33,6 +48,7 @@ class EvalInfer:
         elif self.configs["model_select"] == "xlnet":
             self.model_path = self.configs["model_path"]["xlnet"]
 
+        # 设置模型名称
         self.model_name = self.configs["model_select"]
 
         # 设置评估推断的数据文件
@@ -55,8 +71,10 @@ class EvalInfer:
         elif self.model_name == "xlnet":
             self.tokenizer = XLNetTokenizer.from_pretrained(self.configs["pretrained_model_path"]["xlnet"])
 
+        # 设置label转换器
         self.label_tokenizer = ClassificationLabelTokenizer(load_json(self.label2index_json_path))
 
+        # 加载模型
         self.model = torch.load(self.model_path)
 
     # 预测方法（预测整个数据集）
