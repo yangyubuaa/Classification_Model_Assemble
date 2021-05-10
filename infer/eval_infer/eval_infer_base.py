b027a61814826b964937f2083473c23b46f8a1fc
yangyu <yangyu2019@buaa.edu.cn>
2021-05-10 08:58:03 +0000

    添加测试集预测基类

--- infer/eval_infer/eval_infer_base.py
+++ infer/eval_infer/eval_infer_base.py
@@ -3,7 +3,10 @@ sys.path.append("../")
 
 import torch
 
-from utils.load import load_yaml, load_xlsx
+from utils.load import load_yaml, load_xlsx, load_json
+from transformers import BertTokenizer, ElectraTokenizer, XLNetTokenizer
+from data_preprocess.Tokenizer.tokenizer import SequenceTokenizer
+from data_preprocess.Tokenizer.label_tokenizer import ClassificationLabelTokenizer
 
 # 评估推断方法基类（带标签）
 class EvalInfer:
@@ -30,14 +33,38 @@ class EvalInfer:
         elif self.configs["model_select"] == "xlnet":
             self.model_path = self.configs["model_path"]["xlnet"]
 
+        self.model_name = self.configs["model_select"]
+
         # 设置评估推断的数据文件
         self.eval_data_file = self.configs["eval_data_file"]
 
         # 设置标签转换路径，需要将预测的label转为真实标签
         self.label2index_json_path = self.configs["eval_label_transfer_file"]
 
-    # 预测方法
+        # 设置token映射词表，在自定义模型中的tokenizer使用
+        self.token2index_json_path = self.configs["eval_token_transfer_file"]
+
+        # 设置分词器
+        if self.model_name in ["fasttext", "lstm_base", "lstm_pack", "textcnn"]:
+            self.tokenizer = SequenceTokenizer(load_json(self.token2index_json_path))
+
+        elif self.model_name == "bert":
+            self.tokenizer = BertTokenizer.from_pretrained(self.configs["pretrained_model_path"]["bert"])
+        elif self.model_name == "electra":
+            self.tokenizer = ElectraTokenizer.from_pretrained(self.configs["pretrained_model_path"]["electra"])
+        elif self.model_name == "xlnet":
+            self.tokenizer = XLNetTokenizer.from_pretrained(self.configs["pretrained_model_path"]["xlnet"])
+
+        self.label_tokenizer = ClassificationLabelTokenizer(load_json(self.label2index_json_path))
+
+        self.model = torch.load(self.model_path)
+
+    # 预测方法（预测整个数据集）
     def predict(self):
+        pass
+
+    # 预测一个句子
+    def predict_one(self):
         pass
 
 
